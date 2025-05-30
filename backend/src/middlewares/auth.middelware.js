7c3780f06f70cfe1c2c2e49ab6f68045820bde3c
Dilip Woad <dilipwoad777@gmail.com>
2025-05-31 01:16:55 +0530

    "checking authentication"

--- backend/src/middlewares/auth.middelware.js
+++ backend/src/middlewares/auth.middelware.js
@@ -1,32 +1,35 @@
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const verifyAuthentication = asyncHandler(async(req,res,next)=>{
-    //it will be a middleware
-    //If this middleware will be use to check if user is logged in or not
+const verifyAuthentication = asyncHandler(async (req, res, next) => {
+  //it will be a middleware
+  //If this middleware will be use to check if user is logged in or not
 
-    //this can be check by using access token
-    //We can get the token from the req.cookies or header
+  //this can be check by using access token
+  //We can get the token from the req.cookies or header
 
-    //cookies from web and header from mobile
+  //cookies from web and header from mobile
 
-    //mobile -> "Authorization" : "Bearer accessToken"
-    try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
+  //mobile -> "Authorization" : "Bearer accessToken"
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
 
     //check if token present
-    if(!token){
-        throw new ApiError("Invalid Authorization !!");
+    if (!token) {
+      throw new ApiError("Invalid Authorization !!");
     }
 
     //if token present check if it's valid token or not with jwt
 
-    const jwtPayload = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET_KEY);
-
-    if(!jwtPayload){
-        throw new ApiError("Invalid Access Token");
+    const jwtPayload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+    console.log(jwtPayload);
+    // console.log("AccessToken from cookie/header:", token);
+    if (!jwtPayload) {
+      throw new ApiError("Invalid Access Token");
     }
 
     //if jwtPayload present meaning we get the original payload given at the creation of the access token
@@ -34,21 +37,23 @@ const verifyAuthentication = asyncHandler(async(req,res,next)=>{
 
     //now we have id we can get the user
 
-    const user =await User.findById(jwtPayload?._id).select("-password -refreshToken")
-    if(!user){
-        throw new ApiError("Invalid Access Token!!");
+    const user = await User.findById(jwtPayload?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new ApiError("Invalid Access Token!!");
     }
 
     //if found add this user info(all the info) in the "req" object
 
     req.user = user;
-    console.log("AccessToken from cookie/header:", token);
+    
     //once added to the req obj now work of the middelware is over call the next function
     next();
-    } catch (error) {
-        console.log("At verifying JWT");
-        next(error);
-    }
-})
+  } catch (error) {
+    console.log("At verifying JWT");
+    next(error);
+  }
+});
 
-export {verifyAuthentication};
+export { verifyAuthentication };
