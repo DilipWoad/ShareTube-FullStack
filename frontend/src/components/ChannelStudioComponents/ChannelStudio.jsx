1c59dd596b8667dc629dec44e49206b3cf7f40ad
Dilip Woad <dilipwoad777@gmail.com>
2025-06-04 02:48:21 +0530

    fetech and displayed studio page -> channels videos and channel stats

--- frontend/src/components/ChannelStudioComponents/ChannelStudio.jsx
+++ frontend/src/components/ChannelStudioComponents/ChannelStudio.jsx
@@ -1,34 +1,81 @@
+import axios from "axios";
 import UploadVideo from "../VideoComponents/UploadVideo";
 import StudioVideoCard from "./StudioVideoCard";
+import { BASE_URL } from "../../utils/constant";
+import { useEffect, useState } from "react";
+import {useSelector} from "react-redux";
 
-const ChannelStudio=()=>{
-    return(
-        <div className="flex w-screen h-screen m-2"> 
-            <div className="bg-yellow-300 w-44 text-center mr-4">
-                <div className="bg-lime-200 h-44 mb-4">
-                    <img className="w-44 h-44 rounded-full object-cover" src="http://res.cloudinary.com/dvb5edx52/image/upload/v1739302337/dilip_pic_ox9fuv.jpg" alt="avatar"/>
-                </div>
-                <div className="bg-green-600" >
-                    <div>Dashboard</div>
-                    <div>Edit profile</div>
-                    <div>Videos</div>
-                    <div>Posts</div>
-                </div>
-            </div>
-            <div className="bg-blue-300 flex-grow">
-                <div className="bg-red-500 h-44 mb-4">
-                    <img className="h-44 rounded-xl w-full object-fill" src="http://res.cloudinary.com/dvb5edx52/image/upload/v1739302338/dilip_sig_bty8ug.jpg"/>
-                </div>
-                <div className="bg-orange-400 h-screen mx-2">
-                    <StudioVideoCard/>
-                    <StudioVideoCard/>
-                    <StudioVideoCard/>
-                    <StudioVideoCard/>
-                </div>
-            </div>
-            {/* <UploadVideo uploadBgCss={"bg-black bg-opacity-20"}/> */}
+const ChannelStudio = () => {
+    const[dashboard,setDashboard] =useState(null);
+    const[channelVideos,setChannelVideos] =useState(null);
+    const user = useSelector((store)=>store.user);
+  const getChannelStats = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/dashboard`, { withCredentials: true });
+      setDashboard(res.data.data);
+      console.log(res.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getChannelVideo=async()=>{
+    try {
+        const res= await axios.get(`${BASE_URL}/dashboard/videos`,{withCredentials:true})
+        console.log(res.data.data);
+        setChannelVideos(res.data.data);
+    } catch (error) {
+        console.log(error)
+    }
+  }
+  useEffect(()=>{
+    getChannelStats();
+    getChannelVideo();
+  },[])
+  if(!dashboard) return <div>Loading....</div>
+  return (
+    <div className="flex w-screen h-screen m-2">
+      <div className="bg-yellow-300 w-52 text-center mr-4">
+        <div className="bg-lime-200 flex justify-center h-44 mb-4">
+          <img
+            className="w-44 h-44 rounded-full object-cover"
+            src={user?.avatar}
+            alt="avatar"
+          />
         </div>
-    )
-}
+        <div className="bg-green-600 text-lg">
+          <div className="p-2 bg-gray-400 border-[1px] hover:cursor-pointer hover:bg-gray-500 hover:text-white">
+            Dashboard
+          </div>
+          <div className="p-2 bg-gray-400 border-[1px] hover:cursor-pointer hover:bg-gray-500 hover:text-white">
+            Edit profile
+          </div>
+          <div className="p-2 bg-gray-400 border-[1px] hover:cursor-pointer hover:bg-gray-500 hover:text-white text">
+            Videos
+          </div>
+          <div className="p-2 bg-gray-400 border-[1px] hover:cursor-pointer hover:bg-gray-500 hover:text-white">
+            Posts
+          </div>
+        </div>
+      </div>
+      <div className="bg-blue-300 flex-grow">
+        <div className="bg-red-500 h-44 mb-4">
+          <img
+            className="h-44 rounded-xl w-full object-fill"
+            src={user?.coverImage}
+          />
+        </div>
+        <div className="bg-orange-400 h-screen mx-2">
+          {
+            channelVideos && channelVideos.map((video)=>(
+                <StudioVideoCard channelVideos={video} />
+            ))
+          }
+        </div>
+      </div>
+      {/* <UploadVideo uploadBgCss={"bg-black bg-opacity-20"}/> */}
+    </div>
+  );
+};
 
 export default ChannelStudio;
--- frontend/src/components/ChannelStudioComponents/StudioVideoCard.jsx
+++ frontend/src/components/ChannelStudioComponents/StudioVideoCard.jsx
@@ -1,11 +1,31 @@
-const StudioVideoCard = ()=>{
-    return (
-        <div className="bg-gray-500 h-28 border-y-[1px] items-center flex">
-            <div className="bg-gray-400 h-24 w-full">
-                videos...
-            </div>
+const StudioVideoCard = ({channelVideos}) => {
+    const {thumbnail,views,title} = channelVideos;
+  return (
+    <div className="bg-gray-500 h-28 border-y-[1px] items-center flex rounded-lg">
+      <div className="bg-gray-400 h-24 w-full flex items-center px-2">
+        <input className="mx-2" type="checkbox" />
+        <div className="flex bg-lime-300 w-[750px]">
+          <div className="mr-4">
+            <img
+              className="h-20 w-32 rounded-lg"
+              src={thumbnail}
+              alt="thumbnail"
+            />
+          </div>
+          <div className="text-lg font-semibold">
+            {title}
+          </div>
         </div>
-    )
-}
+        <div className="mx-3 flex items-center justify-center w-24">
+            {views} views
+        </div>
+        
+        <div className ="flex items-center justify-center bg-purple-400 h-full w-20">
+            <input className="" type="checkbox" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default StudioVideoCard;
