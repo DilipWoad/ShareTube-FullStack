56e1c25b03bf0395ccfc65e7ab3269a4fdc07ff9
Dilip Woad <dilipwoad777@gmail.com>
2025-06-10 02:32:02 +0530

    lay loading Channel studio page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,10 +14,13 @@ import ChannelPage from "./components/ChannelComponents/ChannelPage";
 import ChannelVideos from "./components/ChannelComponents/ChannelVideos";
 import ChannelPosts from "./components/ChannelComponents/ChannelPosts";
 import PostComments from "./components/PostComponents/PostComments";
-import ChannelStudio from "./components/ChannelStudioComponents/ChannelStudio";
+// import ChannelStudio from "./components/ChannelStudioComponents/ChannelStudio";
 import StudioVideos from "./components/ChannelStudioComponents/StudioVideos";
 import StudioDashboard from "./components/ChannelStudioComponents/StudioDashboard";
 import UserSubscription from "./components/UserSubscription";
+import { lazy,Suspense } from "react";
+import LoadingScreen from "./utils/LoadingScreen";
+const ChannelStudio = lazy(()=> import('./components/ChannelStudioComponents/ChannelStudio'));
 function App() {
   return (
     <>
@@ -39,7 +42,12 @@ function App() {
                   <Route path="videos" element={<ChannelVideos />} />
                   <Route path="posts" element={<ChannelPosts/>} />
                 </Route>
-                <Route path="/channel/:id/post" element={<PostComments/>}/>
+                <Route path="/channel/:id/post" element={
+                  <Suspense fallback={<LoadingScreen/>}>
+                    <PostComments/>
+                  </Suspense>
+                  
+                  }/>
                 <Route path="/studio" element={<ChannelStudio/>}>
                   <Route index element={<StudioVideos/>}/>
                   <Route path="dashboard" element={<StudioDashboard/>}/>
